279ee2e797213b32f4845156341a604d3c6097e6
IvanBabushkin <moroz.droid@gmail.com>
2020-08-18 18:40:49 +0500

    init

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/IvanBabushkin/terminal/master/install_0.sh)"
-sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/IvanBabushkin/terminal/master/install_1.sh)"
+sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/IvanBabushkin/terminal/master/install_0.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/IvanBabushkin/terminal/master/install_1.sh)"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/IvanBabushkin/terminal/master/install_2.sh)"
