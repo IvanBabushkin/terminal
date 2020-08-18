b9e0d84836777ede14bb232872a53708a026e27c
IvanBabushkin <moroz.droid@gmail.com>
2020-08-18 17:03:13 +0500

    init

--- install.sh
+++ install.sh
@@ -129,7 +129,7 @@ if [ ! -d ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting ]; then
 fi
 
 run_echo "Install oh-my-zsh"
-#sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 #
 curl -fsSL https://raw.githubusercontent.com/IvanBabushkin/terminal/master/zsh/.zshrc > ~/.zshrc
