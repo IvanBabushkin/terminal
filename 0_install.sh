38f7c5ba43c051ec8a8d1276c9758cb54088e8f7
IvanBabushkin <moroz.droid@gmail.com>
2020-08-18 18:39:52 +0500

    init

---
+++ 0_install.sh
@@ -0,0 +1,113 @@
+#!/bin/bash
+set -e
+libeventversion=2.1.11
+tmuxversion=3.1
+
+setup_color() {
+	# Only use colors if connected to a terminal
+		RED=$(printf '\033[31m')
+		GREEN=$(printf '\033[32m')
+		YELLOW=$(printf '\033[33m')
+		BLUE=$(printf '\033[34m')
+		BOLD=$(printf '\033[1m')
+		RESET=$(printf '\033[m')
+}
+
+command_exists() {
+	command -v "$@" >/dev/null 2>&1
+}
+
+error() {
+	echo ${RED}"Error:${RESET} $@" >&2
+}
+
+run_echo() {
+    echo ${BLUE}"Run:${RESET} $@" >&2
+}
+
+setup_color
+
+if [ "$EUID" -ne 0 ]
+  error "Please run as root"
+  exit 1
+fi
+
+if [ -f /etc/redhat-release ]; then
+  run_echo "Ok it's CentOS, go .."
+else
+    error "CentOS only"
+    exit 1
+fi
+
+run_echo "Install Deps"
+sudo yum install -y gcc kernel-devel make ncurses-devel git wget curl nano tar zsh> /dev/null 2>&1 || {
+    error "make install Deps"
+    exit 1
+}
+
+if [[ $(tmux -V) == "tmux $tmuxversion" ]]
+then
+    run_echo Tmux $tmuxversion installed
+else
+    sudo rm -rf ~/Downloads/tmux > /dev/null 2>&1
+    sudo mkdir -p ~/Downloads/tmux > /dev/null 2>&1
+
+    run_echo "Download and install libevent"
+    sudo cd ~/Downloads/tmux
+
+    curl -OL "https://github.com/libevent/libevent/releases/download/release-$libeventversion-stable/libevent-$libeventversion-stable.tar.gz" >/dev/null 2>&1 || {
+        error "Download libevent"
+        exit 1
+    }
+    tar -xvzf "libevent-$libeventversion-stable.tar.gz" >/dev/null 2>&1 || {
+        error "Tar libevent"
+        exit 1
+    }
+    cd "libevent-$libeventversion-stable" >/dev/null 2>&1
+    ./configure --prefix=/usr/local >/dev/null 2>&1 || {
+        error "configure libevent"
+        exit 1
+    }
+    sudo make >/dev/null 2>&1 || {
+        error "make libevent"
+        exit 1
+    }
+    sudo make install >/dev/null 2>&1 || {
+        error "make install libevent"
+        exit 1
+    }
+
+    run_echo "Download and install Tmux"
+    sudo cd ~/Downloads/tmux
+    curl -OL "https://github.com/tmux/tmux/releases/download/$tmuxversion/tmux-$tmuxversion.tar.gz" >/dev/null 2>&1 || {
+        error "download tmux"
+        exit 1
+    }
+    tar -xvzf "tmux-$tmuxversion.tar.gz" >/dev/null 2>&1 || {
+        error "tar tmux"
+        exit 1
+    }
+    cd "tmux-$tmuxversion" >/dev/null 2>&1 || {
+        exit 1
+    }
+    sudo LDFLAGS="-L/usr/local/lib -Wl,-rpath=/usr/local/lib" ./configure --prefix=/usr >/dev/null 2>&1 || {
+        error "configure tmux"
+        exit 1
+    }
+    sudo make >/dev/null 2>&1 || {
+        error "make tmux"
+        exit 1
+    }
+    sudo make install >/dev/null 2>&1 || {
+        error "make install tmux"
+        exit 1
+    }
+fi
+
+if [[ $(tmux -V) == "tmux $tmuxversion" ]]
+then
+    run_echo "Tmux $tmuxversion installed"
+else
+    error "Tmux $tmuxversion not installed"
+fi
+
---
+++ 2_install.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+set -e
+
+libeventversion=2.1.11
+tmuxversion=3.1
+
+setup_color() {
+	# Only use colors if connected to a terminal
+		RED=$(printf '\033[31m')
+		GREEN=$(printf '\033[32m')
+		YELLOW=$(printf '\033[33m')
+		BLUE=$(printf '\033[34m')
+		BOLD=$(printf '\033[1m')
+		RESET=$(printf '\033[m')
+}
+
+command_exists() {
+	command -v "$@" >/dev/null 2>&1
+}
+
+error() {
+	echo ${RED}"Error:${RESET} $@" >&2
+}
+
+run_echo() {
+    echo ${BLUE}"Run:${RESET} $@" >&2
+}
+
+setup_color
+
+if [ ! -f /etc/redhat-release ]; then
+    error "CentOS only"
+    exit 1
+fi
+
+
+run_echo "Install oh-my-zsh plugins"
+mkdir -p ~/.oh-my-zsh/custom/plugins/
+
+if [ ! -d ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions ]; then
+    cd ~/.oh-my-zsh/custom/plugins/
+    git clone https://github.com/zsh-users/zsh-autosuggestions >/dev/null 2>&1 || {
+        error "install zsh-autosuggestions"
+        exit 1
+    }
+fi
+
+if [ ! -d ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting ]; then
+    cd ~/.oh-my-zsh/custom/plugins/
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting  >/dev/null 2>&1 || {
+        error "zsh-syntax-highlighting"
+        exit 1
+    }
+fi
+
+run_echo "Install zsh config"
+curl -fsSL https://raw.githubusercontent.com/IvanBabushkin/terminal/master/zsh/.zshrc > ~/.zshrc
+run_echo "Install zsh theme"
+mkdir -p ~/.oh-my-zsh/custom/themes/
+curl -fsSL https://raw.githubusercontent.com/IvanBabushkin/terminal/master/zsh/theme/me.zsh-theme > ~/.oh-my-zsh/custom/themes/me.zsh-theme
+
+run_echo "Install Tmux Plugin Manager"
+if [ ! -d ~/.tmux/plugins/tpm ]; then
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
+
+run_echo "Install Tmux config"
+curl -fsSL https://raw.githubusercontent.com/IvanBabushkin/terminal/master/tmux/.tmux.conf > ~/.tmux.conf
+run_echo "Install bash config"
+curl -fsSL https://raw.githubusercontent.com/IvanBabushkin/terminal/master/bashrc/.bashrc > ~/.bashrc
+run_echo "plz reload bash and plugins: source ~/.bashrc"
+
+
+# rm -rf ~/.oh-my-zsh
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/IvanBabushkin/terminal/master/install.sh)"
