ba0da2523bf740070851746eb0000af6ec432c85
IvanBabushkin <moroz.droid@gmail.com>
2020-08-18 19:15:10 +0500

    init

--- 1_install.sh
+++ 1_install.sh
@@ -4,6 +4,30 @@ set -e
 libeventversion=2.1.11
 tmuxversion=3.1
 
+setup_color() {
+	# Only use colors if connected to a terminal
+		RED=$(printf '\033[31m')
+		GREEN=$(printf '\033[32m')
+		YELLOW=$(printf '\033[33m')
+		BLUE=$(printf '\033[34m')
+		BOLD=$(printf '\033[1m')
+		RESET=$(printf '\033[m')
+}
+
+command_exists() {
+	command -v "$@" >/dev/null 2>&1
+}
+
+error() {
+	echo ${RED}"Error:${RESET} $@" >&2
+}
+
+run_echo() {
+    echo ${BLUE}"Run:${RESET} $@" >&2
+}
+
+setup_color
+
 if [ ! -f /etc/redhat-release ]; then
     error "CentOS only"
     exit 1
